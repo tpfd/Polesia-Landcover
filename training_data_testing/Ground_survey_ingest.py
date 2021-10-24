acc0a4b94929807ad10df5951937090cbfd9789f
Thomas Dowling <tpfdowling@gmail.com>
2021-10-24 13:36:20 +0100

    New training data

--- training_data_testing/Ground_survey_ingest.py
+++ training_data_testing/Ground_survey_ingest.py
@@ -5,10 +5,14 @@ Quick script to parse in ground survey data and return a compound table
 import pandas as pd
 from pathlib import Path
 
-# Read in
+# Top directory in which all tables are located
 dir_path = "D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Field_surveys/"
+
+# Set the names of the columns in the output table
 df_out = pd.DataFrame(columns=['Lat', 'Lon', 'Type', 'Square_ID'])
 
+# Get the columns and rows in which you are interested
+# Skips the first row in order to get data needed
 for path in Path(dir_path).rglob('*.xlsx'):
     print('Reading:', path.name)
     df_in = pd.read_excel(path, skiprows=[0])
@@ -18,6 +22,6 @@ for path in Path(dir_path).rglob('*.xlsx'):
                             'Square_ID': df_in.iloc[0, [8]].iloc[0]},
                            ignore_index=True)
 
-# Export
+# Exports to top directory
 df_out.to_csv(dir_path+'Ground_surveys.csv')
 print('Record exported')
--- training_data_testing/Training_point_examination.py
+++ training_data_testing/Training_point_examination.py
@@ -4,42 +4,82 @@ Script to test the training points and balance the classes
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-
-# Load shapefile
-fpath = "D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Training_data/Training_points_20_20.shp"
+"""
+Complex swamps
+"""
+# Load shapefile complex
+fpath = "D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Training_data/Complex_points.shp"
 raw_df = gpd.read_file(fpath)
 
 class_count = raw_df['VALUE'].value_counts()
-class_count.plot(kind='bar', legend=False, title='Training points - all')
+class_count.plot(kind='bar', legend=False, title='Complex training points - all')
 plt.show()
 
 # Re-sample to 500 samples
-sample_amounts = {0: 500,
-                  1: 500,
-                  2: 500,
-                  3: 500,
-                  4: 500,
-                  5: 500,
-                  6: 500,
-                  7: 500,
-                  8: 500,
-                  9: 500,
-                  10: 500,
-                  11: 500,
-                  12: 500,
-                  13: 500,
-                  14: 500,
-                  15: 500,
-                  16: 500,
-                  17: 500,
-                  18: 500,
-                  19: 500}
+sample_amounts = {0: 1000,
+                  1: 1000,
+                  2: 1000,
+                  3: 1000,
+                  4: 1000,
+                  5: 1000,
+                  6: 1000,
+                  7: 1000,
+                  8: 1000,
+                  9: 1000,
+                  11: 1000,
+                  12: 1000,
+                  13: 1000,
+                  14: 1000,
+                  15: 1000,
+                  16: 1000,
+                  17: 1000,
+                  18: 1000,
+                  19: 1000}
 
 resampled_df = (raw_df.groupby('VALUE').apply(lambda g: g.sample(n=sample_amounts[g.name],
                                                                  replace=len(g) < sample_amounts[g.name])).droplevel(0))
 class_count = resampled_df['VALUE'].value_counts()
-class_count.plot(kind='bar', legend=False, title='Training points - resampled')
+class_count.plot(kind='bar', legend=False, title='Complex training points - resampled')
 plt.show()
 
 # Export to new .shp
-resampled_df.to_file("D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Training_data/Training_points_500.shp")
+resampled_df.to_file("D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Training_data/Complex_points_500.shp")
+
+
+"""
+Simple swamps
+"""
+fpath = "D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Training_data/Simple_points.shp"
+raw_df = gpd.read_file(fpath)
+
+class_count = raw_df['VALUE'].value_counts()
+class_count.plot(kind='bar', legend=False, title='Simple training points - all')
+plt.show()
+
+# Re-sample to 500 samples
+sample_amounts = {0: 1000,
+                  1: 1000,
+                  2: 1000,
+                  3: 1000,
+                  4: 1000,
+                  5: 1000,
+                  6: 1000,
+                  7: 1000,
+                  8: 1000,
+                  9: 1000,
+                  11: 1000,
+                  12: 1000,
+                  13: 1000,
+                  10: 1000,
+                  17: 1000,
+                  18: 1000,
+                  19: 1000}
+
+resampled_df = (raw_df.groupby('VALUE').apply(lambda g: g.sample(n=sample_amounts[g.name],
+                                                                 replace=len(g) < sample_amounts[g.name])).droplevel(0))
+class_count = resampled_df['VALUE'].value_counts()
+class_count.plot(kind='bar', legend=False, title='Simple training points - resampled')
+plt.show()
+
+# Export to new .shp
+resampled_df.to_file("D:/tpfdo/Documents/Artio_drive/Projects/Polesia/Training_data/Simple_points_500.shp")
