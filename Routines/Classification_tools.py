c0b9c4af441718e1f486f64cf3573eca8527c588
Thomas Dowling <tpfdowling@gmail.com>
2021-12-05 22:42:35 +0000

    Update Classification_tools.py

--- Routines/Classification_tools.py
+++ Routines/Classification_tools.py
@@ -56,10 +56,12 @@ def map_target_area(fp_target_ext, fp_export_dir, years_to_map, scale, clf_compl
                     print(e)
                     continue
 
-        shutil.rmtree(tile_dir)
-
     # Clean up
-    shutil.rmtree(process_dir)
+    try:
+        shutil.rmtree(process_dir)
+    except Exception as e:
+        print('Failed to clear tile directory (try doing it manually) with error:')
+        print(e)
     print('map_target_area(): bye!')
 
 
