39b296780c8fa0e3fc939ab8cd841fd1b872d819
Thomas Dowling <tpfdowling@gmail.com>
2023-06-27 19:54:47 +1200

    Introducing the Sentinel wetness functions

--- Routines/Wetness_extraction.py
+++ Routines/Wetness_extraction.py
@@ -1,3 +1,13 @@
+"""
+Test location:
+lon_test = 24.113395
+lat_test = 52.6448816667
+date_test = '2017-07-16'
+
+get_averages_for_nests(lon_test, lat_test, date_test)
+get_sentinel_wetness(lon_test, lat_test, date_test, base_dir)
+
+"""
 import sys
 import ee
 import pandas as pd
@@ -7,21 +17,31 @@ from Wetness_functions import*
 
 # Set dirs
 base_dir = 'C:/Users/tdow214/OneDrive - The University of Auckland/Documents/Projects/WetEagles'
-fp_target_flight_pts = f"{base_dir}/point_data.csv"
-fp_target_nest_pts = f"{base_dir}/point_data.csv"
-fp_out_data = f"{base_dir}/extracted_data.csv"
+fp_target_flight_pts = f"{base_dir}/point_data_movement_v1.csv"
+fp_target_nest_pts = f"{base_dir}/point_data_nests.csv"
+fp_out_data_movement = f"{base_dir}/extracted_data_movement.csv"
 
-# Load list of eagle nest points into a multi-index df
-eagle_df = pd.read_csv(fp_target_nest_pts)
-eagle_df = eagle_df.set_index([pd.to_datetime(eagle_df['date']), 'event.id'], inplace=False)
 
-# Run df through data collect from earth engine and export to csv
-for index, row in eagle_df.iterrows():
-    averages = get_averages(row['location.long'], row['location.lat'], row['date'])
+# Run df through data collect from earth engine and export to csv for nests
+eagle_nests_df = pd.read_csv(fp_target_nest_pts)
+eagle_nests_df = eagle_nests_df.set_index([pd.to_datetime(eagle_nests_df['date']), 'event.id'], inplace=False)
+
+for index, row in eagle_nests_df.iterrows():
+    averages = get_averages_for_nests(row['location.long'], row['location.lat'], row['date'])
     save_data_to_csv(row, averages, fp_out_data)
     print('Appended:', index, averages)
+print('Done nest data extraction!')
 
 
-
-
-
+# Run movement data through earth engine and export to csv
+eagle_movement_df = pd.read_csv(fp_target_flight_pts)
+eagle_movement_df = eagle_movement_df.set_index([pd.to_datetime(eagle_movement_df['date']), 'event.id'], inplace=False)
+for index, row in eagle_movement_df.iterrows():
+    print(row['location.long'], row['location.lat'], row['date'])
+    sentinel_out = get_sentinel_wetness(row['location.long'], row['location.lat'], row['date'])
+    new_row_dict = {'index': index,
+                    'S1_SM': sentinel_out['S1 Soil Moisture'],
+                    'S1_binary': sentinel_out['S1 Surface Water binary'],
+                    'S2_inundation':sentinel_out['S2 Inundation binary']}
+    save_dict_to_csv(new_row_dict, fp_out_data_movement)
+print('Done movement data extraction!')
--- Routines/Wetness_functions.py
+++ Routines/Wetness_functions.py
@@ -1,10 +1,91 @@
 import ee
 import csv
-import pandas as pd
+import os
 import numpy as np
 
 
-def get_averages(latitude, longitude, date):
+def calculate_surface_water(image):
+    # Apply threshold to classify surface water below tree canopy
+    tree_canopy = image.lt(-20)  # Example threshold value, adjust as needed
+    surface_water = image.multiply(tree_canopy)
+    return surface_water.rename('water')
+
+
+def calculate_inundation(image):
+    ndwi = image.normalizedDifference(['B3', 'B8']).rename('NDWI')
+    threshold = 0.2
+    inundated = ndwi.gt(threshold)
+    return inundated
+
+
+def get_sentinel_wetness(longitude, latitude, date):
+    """
+    longitude = 24.1325316667
+    latitude = 52.7465083333
+    date = '2017-07-05'
+
+    indunation binary, 0 = no water visible (but cannot see through canopy)
+    """
+    # Set the point of interest (longitude, latitude)
+    point = ee.Geometry.Point(longitude, latitude)
+
+    # Convert date to Earth Engine's format and get the 3-day period around it
+    start_date = ee.Date(date).advance(-1, 'day')
+    end_date = ee.Date(date).advance(1, 'day')
+
+    # Load Sentinel-1 Synthetic Aperture Radar (SAR) collection
+    sentinel1 = ee.ImageCollection('COPERNICUS/S1_GRD') \
+        .filterBounds(point) \
+        .filterDate(start_date, end_date) \
+        .select('VV')
+
+    if sentinel1.size().getInfo() > 0:
+        # Calculate SM using an alpha approximation
+        mean_image = sentinel1.mean()
+        soil_moisture = mean_image.expression('(10**((VV*0.1) + 1.3)) / 100.0', {'VV': mean_image})
+        soil_moisture_value = soil_moisture.sample(point, scale=10).first().get('constant')
+        soil_moisture_value_out = np.around(soil_moisture_value.getInfo(), decimals=3)
+
+        # Calculate binary surface water using arbitrary canopy threshold
+        surface_water = sentinel1.map(calculate_surface_water).sum()
+        surface_water_value = surface_water.reduceRegion(
+            reducer=ee.Reducer.mean(),
+            geometry=point,
+            scale=10).get('water')
+        surface_water_value_out = surface_water_value.getInfo()
+    else:
+        soil_moisture_value_out = np.nan
+        surface_water_value_out = np.nan
+
+    # Load Sentinel-2 optical image collection
+    sentinel2 = ee.ImageCollection('COPERNICUS/S2') \
+        .filterBounds(point) \
+        .filterDate(start_date, end_date)
+
+    if sentinel2.size().getInfo() >= 0:
+        # Calculate frequency of inundation using Sentinel-2
+        inundation = sentinel2.map(calculate_inundation).sum()
+
+        # Get the inundation binary value for the given point
+        inundation_value = inundation.reduceRegion(
+            reducer=ee.Reducer.mean(),
+            geometry=point,
+            scale=10).get('NDWI')
+        inundation_value_out= inundation_value.getInfo()
+    else:
+        inundation_value_out = np.nan
+
+
+    result = {
+        'S1 Soil Moisture': soil_moisture_value_out,
+        'S1 Surface Water binary': surface_water_value_out,
+        'S2 Inundation binary': inundation_value_out
+    }
+    print(result)
+    return result
+
+
+def get_averages_for_nests(latitude, longitude, date):
     """
     https://developers.google.com/earth-engine/datasets/catalog/NASA_GPM_L3_IMERG_V06#bands
     https://developers.google.com/earth-engine/datasets/catalog/NASA_SMAP_SPL4SMGP_007#description
@@ -29,7 +110,7 @@ def get_averages(latitude, longitude, date):
     soil_moisture_filtered = soil_moisture.filterDate(start_date, end_date)
 
     # Check for data presence and extract the mean if there is data, otherwise set to np.nan
-    if rainfall_filtered.size().getInfo() >= 0:
+    if rainfall_filtered.size().getInfo() > 0:
         rainfall_filtered_mean = rainfall_filtered.mean().reduceRegion(ee.Reducer.mean(),
                                                                        geometry=buffer, scale=10000, crs='EPSG:4326',
                                                                        bestEffort=True).get('precipitationCal')
@@ -40,7 +121,7 @@ def get_averages(latitude, longitude, date):
     else:
         rainfall_mean = np.nan
 
-    if soil_moisture_filtered.size().getInfo() >= 0:
+    if soil_moisture_filtered.size().getInfo() > 0:
         soil_moisture_mean = soil_moisture_filtered.mean().reduceRegion(ee.Reducer.mean(),
                                                                         geometry=buffer, scale=10000, crs='EPSG:4326',
                                                                         bestEffort=True).get('sm_rootzone_wetness')
@@ -57,30 +138,13 @@ def get_averages(latitude, longitude, date):
     }
 
 
-def save_data_to_csv(row, averages, file_path):
-    """
-    Apologies for the hack job... I'm tired and hungry.
-    Here is where the use of the multi-index to make a clean transition is supposed to happen.
-    Instead, I have done some pandas hokey-pokey.
-    """
-    new_row = pd.DataFrame(averages, index=[0]).transpose()
-    row_df = pd.Series.to_frame(row)
-    row_out = pd.concat([new_row, row_df.loc[:]]).reset_index(drop=False)
-    values_to_move = row_out.iloc[:2, 1].tolist()
-    row_out.iloc[:2, 2] = values_to_move
-    row_out = row_out.drop(row_out.columns[1], axis=1)
-    row_out.iloc[2, 0] = 'ID'
+def save_dict_to_csv(data_dict, filename):
+    file_exists = os.path.isfile(filename)
 
-    values = row_out.iloc[:, 1].tolist()
-    header = row_out['index'].tolist()
+    with open(filename, 'a', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data_dict.keys())
 
-    if not pd.io.common.file_exists(file_path):
-        mode = 'w'  # Create a new file if it doesn't exist
-    else:
-        mode = 'a'  # Append to existing file
+        if not file_exists:
+            writer.writeheader()
 
-    with open(file_path, mode, newline='') as file:
-        writer = csv.writer(file)
-        if mode == 'w':  # Write header if the file is new
-            writer.writerow(header)
-        writer.writerow(values)
+        writer.writerow(data_dict)
