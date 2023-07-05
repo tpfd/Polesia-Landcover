d22fc3c734cf4f97daed33aede3d5006b5f83f37
Thomas Dowling <tpfdowling@gmail.com>
2023-07-06 10:29:09 +1200

    Data run 1 for Wet Eagles

--- Routines/Wetness_extraction.py
+++ Routines/Wetness_extraction.py
@@ -1,34 +1,47 @@
 """
-Test location:
-lon_test = 24.113395
-lat_test = 52.6448816667
-date_test = '2017-07-16'
-
-get_averages_for_nests(lon_test, lat_test, date_test)
-get_sentinel_wetness(lon_test, lat_test, date_test, base_dir)
-
+Main run script for wetness extraction.
 """
 import sys
 import ee
 import pandas as pd
-
-ee.Initialize()
 sys.path.append("C:/Users/tdow214/OneDrive - The University of Auckland/Documents/GitHub/Polesia-Landcover/Routines/")
 from Wetness_functions import *
 
 # Set dirs
+ee.Initialize()
 base_dir = 'C:/Users/tdow214/OneDrive - The University of Auckland/Documents/Projects/WetEagles'
-fp_target_flight_pts = f"{base_dir}/point_data_movement_v1.csv"
-fp_target_nest_pts = f"{base_dir}/point_data_nests.csv"
+fp_target_flight_pts = f"{base_dir}/all point data.csv"
+fp_target_nest_pts = f"{base_dir}/nest locations.csv"
 fp_out_data_movement = f"{base_dir}/extracted_data_movement.csv"
+fp_out_nest = f"{base_dir}/extracted_nest_conditions.csv"
+
+# Run the nests data through earth engine and export to csv
+eagle_nest_df = pd.read_csv(fp_target_nest_pts)
+eagle_nest_df.rename(columns={eagle_nest_df.columns[0]: "id"}, inplace=True)
+print('Starting nest extraction...')
+for index, row in eagle_nest_df.iterrows():
+    # Analyse the nest locations
+    new_index_in = str(int(row['year'])) + '_' + str(int(row['id']))
+    get_averages_for_nests(row['latitude'],
+                           row['longitude'],
+                           row['year'],
+                           row['radius_km'],
+                           new_index_in,
+                           fp_out_nest)
+print('Nest extraction done!')
 
 # Run movement data through earth engine and export to csv
 eagle_movement_df = pd.read_csv(fp_target_flight_pts)
 eagle_movement_df = eagle_movement_df.set_index([pd.to_datetime(eagle_movement_df['date']), 'event.id'], inplace=False)
+print('Starting movement extraction...')
 for index, row in eagle_movement_df.iterrows():
-    print(row['location.long'], row['location.lat'], row['date'])
     # Analyse the movement location points
-    sentinel_out = get_sentinel_wetness(row['location.long'], row['location.lat'], row['date'], base_dir, None)
+    print('Getting movement data for:', index, row['date'])
+    sentinel_out = get_sentinel_wetness(row['location.long'],
+                                        row['location.lat'],
+                                        row['date'],
+                                        base_dir,
+                                        None)
     sen_new_row_dict = {'index': index,
                         'S1 Soil Moisture': sentinel_out['S1 Soil Moisture'],
                         'S1 total pixel count': sentinel_out['S1 total pixel count'],
@@ -36,19 +49,4 @@ for index, row in eagle_movement_df.iterrows():
                         'S2 total pixel count': sentinel_out['S2 total pixel count'],
                         'S2 Inundation count': sentinel_out['S2 Inundation count']}
     save_dict_to_csv(sen_new_row_dict, fp_out_data_movement)
-
-# Run the nests data through earth engine and export to csv
-eagle_nest_df = pd.read_csv(fp_target_nest_pts)
-eagle_nest_df = eagle_nest_df.set_index([pd.to_datetime(eagle_nest_df['date']), 'event.id'], inplace=False)
-for index, row in eagle_nest_df.iterrows():
-    # Analyse the nest locations
-    nests_out = get_averages_for_nests(row['location.lat'], row['location.long'], row['date'])
-    sen_new_row_dict = {'index': index,
-                        'S1 Soil Moisture': sentinel_out['S1 Soil Moisture'],
-                        'S1 total pixel count': sentinel_out['S1 total pixel count'],
-                        'S1 Inundation count': sentinel_out['S1 Inundation count'],
-                        'S2 total pixel count': sentinel_out['S2 total pixel count'],
-                        'S2 Inundation count': sentinel_out['S2 Inundation count']}
-    save_dict_to_csv(sen_new_row_dict, fp_out_data_movement)
-
-print('Done data extraction!')
+print('Movement extraction done!')
--- Routines/Wetness_functions.py
+++ Routines/Wetness_functions.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import date, timedelta
 
 
 def count_non_nan_pixels(image, selector):
@@ -101,7 +102,7 @@ def get_pixel_counts_from_hist(x_in, y_in):
     return int(total_pixel_count), int(inundation_count), water_flag
 
 
-def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
+def get_sentinel_wetness(longitude, latitude, date_in, base_dir, site_name):
     """
     longitude = 24.1325316667
     latitude = 52.7465083333
@@ -113,7 +114,7 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
     S1 10 m pixel size: https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_S1_GRD
     S2 10 m pixel size: https://developers.google.com/earth-engine/datasets/catalog/COPERNICUS_S2_SR_HARMONIZED
 
-    If you pass a site name, it will save histograms.
+    If you pass a site name, it will save histograms. Pass None to just process silently.
     """
     # Set the point of interest (longitude, latitude)
     point = ee.Geometry.Point(longitude, latitude)
@@ -123,8 +124,8 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
     buffer = point.buffer(buffer_radius)
 
     # Convert date to Earth Engine's format and get the 3-day period around it
-    start_date = ee.Date(date).advance(-1, 'day')
-    end_date = ee.Date(date).advance(1, 'day')
+    start_date = ee.Date(date_in).advance(-1, 'day')
+    end_date = ee.Date(date_in).advance(1, 'day')
 
     # Load Sentinel-1 Synthetic Aperture Radar (SAR) collection
     s1_collection = ee.ImageCollection('COPERNICUS/S1_GRD') \
@@ -134,7 +135,6 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
 
     if s1_collection.size().getInfo() > 0:
         # Carry out pre-processing
-        print('De-speckling S1...')
         s1_mean_image = s1_collection.mean().clip(buffer)
         vv = s1_mean_image.select('VV')
         vv_smoothed = vv.focal_median(30, 'circle', 'meters').rename('VV_Filtered')  # De-speckle
@@ -148,11 +148,9 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
                 pass
 
             # Calculate SM using an alpha approximation on the temporal mean
-            print('Calculating SM from S1...')
             soil_moisture_value_out = compute_soil_moisture(vv_smoothed, buffer)
 
             # Calculate binary surface water
-            print('Calculating surface water from S1...')
             s1_masked_image, s1_water_threshold = calculate_water_under_canopy(vv_smoothed)
             s1_x, s1_y = compute_histogram(s1_masked_image.select('S1 Surface Water Binary'),
                                            buffer,
@@ -165,13 +163,11 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
                 s1_inundation_count = 0
 
         else:
-            print('S1 has no non-nan pixels!')
             soil_moisture_value_out = np.nan
             s1_total_pixel_count = np.nan
             s1_inundation_count = np.nan
 
     else:
-        print('No S1 available')
         soil_moisture_value_out = np.nan
         s1_total_pixel_count = np.nan
         s1_inundation_count = np.nan
@@ -183,13 +179,11 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
 
     if sentinel2.size().getInfo() > 0:
         # Cloud mask
-        print('Masking cloud S2...')
         s2_cloud_filtered = s2_cloud_masking(sentinel2, buffer)
 
         s2_total_pixel_count = count_non_nan_pixels(s2_cloud_filtered, 'B3')
         if s2_total_pixel_count > 0:
             # Calculate inundation binary using Sentinel-2
-            print('Calculating S2 inundation...')
             s2_inundation, ndwi_raw, ndwi_threshold = calculate_s2_inundation(s2_cloud_filtered)
             s2_x, s2_y = compute_histogram(s2_inundation.select('S2 Surface Water Binary'),
                                            buffer,
@@ -210,11 +204,9 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
             else:
                 pass
         else:
-            print('S2 is all cloud!')
             s2_inundation_count = np.nan
             s2_total_pixel_count = np.nan
     else:
-        print('No S2 available')
         s2_inundation_count = np.nan
         s2_total_pixel_count = np.nan
 
@@ -225,12 +217,24 @@ def get_sentinel_wetness(longitude, latitude, date, base_dir, site_name):
         'S2 total pixel count': s2_total_pixel_count,
         'S2 Inundation count': s2_inundation_count
     }
-    pprint.pprint(result)
+    pprint(result)
     return result
 
 
-def get_averages_for_nests(latitude, longitude, date, home_range):
+def get_month_dates(year):
+    month_dates = []
+    current_date = date(year, 1, 1)
+    while current_date.year == year:
+        next_month = current_date.replace(day=28) + timedelta(days=4)  # Ensure we move to the next month
+        month_end = next_month - timedelta(days=next_month.day)
+        month_dates.append((current_date.strftime('%Y-%m-%d'), month_end.strftime('%Y-%m-%d')))
+        current_date = next_month
+    return month_dates
+
+
+def get_averages_for_nests(latitude, longitude, year, home_range, index_in, fp_out_nest):
     """
+    Assumes a year is given as a single YYYY int, returns monthly means for that year.
     https://developers.google.com/earth-engine/datasets/catalog/NASA_GPM_L3_IMERG_V06#bands
     https://developers.google.com/earth-engine/datasets/catalog/NASA_SMAP_SPL4SMGP_007#description
     """
@@ -241,50 +245,55 @@ def get_averages_for_nests(latitude, longitude, date, home_range):
     # Create a point from the input coordinates
     point = ee.Geometry.Point(longitude, latitude)
 
-    # Create a 25 km buffer around the point
+    # Create a buffer around the point
     buffer_radius = home_range
     buffer = point.buffer(buffer_radius)
 
-    # Convert date to Earth Engine's format and get the 3-day period around it
-    start_date = ee.Date(date).advance(-1, 'day')
-    end_date = ee.Date(date).advance(1, 'day')
+    # Get monthly date ranges for the year and run them
+    dates_list = get_month_dates(int(year))
+    for date_pair in dates_list:
+        index_out = index_in + '_' + date_pair[0].split('-')[1]
+        start_date = ee.Date(date_pair[0])
+        end_date = ee.Date(date_pair[1])
+        print('Getting nest data for:', index_out)
 
-    # Filter datasets by date
-    rainfall_filtered = rainfall.filterDate(start_date, end_date)
-    soil_moisture_filtered = soil_moisture.filterDate(start_date, end_date)
+        # Filter datasets by date
+        rainfall_filtered = rainfall.filterDate(start_date, end_date)
+        soil_moisture_filtered = soil_moisture.filterDate(start_date, end_date)
 
-    # Check for data presence and extract the mean if there is data, otherwise set to np.nan
-    if rainfall_filtered.size().getInfo() > 0:
-        rainfall_filtered_mean = rainfall_filtered.mean().reduceRegion(ee.Reducer.mean(),
-                                                                       geometry=buffer, scale=10000, crs='EPSG:4326',
-                                                                       bestEffort=True).get('precipitationCal')
-        if rainfall_filtered_mean.getInfo() is None:
+        # Check for data presence and extract the mean if there is data, otherwise set to np.nan
+        if rainfall_filtered.size().getInfo() > 0:
+            rainfall_filtered_mean = rainfall_filtered.mean().reduceRegion(ee.Reducer.mean(),
+                                                                           geometry=buffer, scale=10000, crs='EPSG:4326',
+                                                                           bestEffort=True).get('precipitationCal')
+            if rainfall_filtered_mean.getInfo() is None:
+                rainfall_mean = np.nan
+            else:
+                rainfall_mean = np.around(rainfall_filtered_mean.getInfo(), decimals=4)
+        else:
             rainfall_mean = np.nan
-        else:
-            rainfall_mean = np.around(rainfall_filtered_mean.getInfo(), decimals=4)
-    else:
-        rainfall_mean = np.nan
 
-    if soil_moisture_filtered.size().getInfo() > 0:
-        soil_moisture_mean = soil_moisture_filtered.mean().reduceRegion(ee.Reducer.mean(),
-                                                                        geometry=buffer, scale=10000, crs='EPSG:4326',
-                                                                        bestEffort=True).get('sm_rootzone_wetness')
-        if soil_moisture_mean.getInfo() is None:
+        if soil_moisture_filtered.size().getInfo() > 0:
+            soil_moisture_mean = soil_moisture_filtered.mean().reduceRegion(ee.Reducer.mean(),
+                                                                            geometry=buffer, scale=10000, crs='EPSG:4326',
+                                                                            bestEffort=True).get('sm_rootzone_wetness')
+            if soil_moisture_mean.getInfo() is None:
+                soil_moisture_mean = np.nan
+            else:
+                soil_moisture_mean = np.around(soil_moisture_mean.getInfo(), decimals=4)
+        else:
             soil_moisture_mean = np.nan
-        else:
-            soil_moisture_mean = np.around(soil_moisture_mean.getInfo(), decimals=4)
-    else:
-        soil_moisture_mean = np.nan
 
-    result = {'Soil Moisture': soil_moisture_mean,
-              'Precipitation': rainfall_mean}
-    pprint.pprint(result)
-    return result
+        nests_new_row_dict = {'index': index_out,
+                              'SM rootzone wetness': soil_moisture_mean,
+                              'Precipitation': rainfall_mean}
+        pprint(nests_new_row_dict)
+        save_dict_to_csv(nests_new_row_dict, fp_out_nest)
+    return
 
 
 def save_dict_to_csv(data_dict, filename):
     file_exists = os.path.isfile(filename)
-
     with open(filename, 'a', newline='') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=data_dict.keys())
 
